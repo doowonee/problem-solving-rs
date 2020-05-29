d3bca4b2a889563e044c663d71752723ab26f904
the1900 <the1900.me@gmail.com>
2020-05-29 14:46:40 +0900

    임시
    도형과 블럭을 배열이 아니라 coordinate point의 리스트로 표한하고 연산 하고 싶다.
    근데 tuple에 tupel을 어캐 하는지 모르겠는데; const도 안되는디..

--- src/puzzle.rs
+++ src/puzzle.rs
@@ -1,7 +1,15 @@
 //! Find the minimum count of blocks to fit in given shape.
 //! 
+//! Gien shape an blocks are represeted as coordiate point see https://www.mathworks.com/help/images/image-coordinate-systems.html
+//! 
 //! ```
-//! block sizes are 1*1, 1*2, 1*3, 1*4, 2*2.
+//! block types are .
+//! 
+//! 1x1 (0,0)
+//! 1x2 (0,0), (1,0)
+//! 1x3 (0,0), (1,0), (2,0)
+//! 1x4 (0,0), (1,0), (2,0) (3,0)
+//! 2x2 (0,0), (1,0), (1,1) (0,1)
 //! 
 //! If given problem is
 //! 
@@ -11,6 +19,8 @@
 //!     [0, 1, 1, 1],
 //!     [0, 1, 0, 0]
 //! ]
+//! 
+//! As corodinate system it's ((0,0), (1,0), (1.1), (1,2), (1,3), (2,2), (3,2))
 //! 
 //! then minumum is 3 like below
 //! 
@@ -39,18 +49,21 @@
 //! ```
 //! 
 
+// 블럭 종류를 const로 포인트의 리스트를 하나의 블록이라 하고 블록 종류는 리스트로 못만드나?
+// tuple의 tuple 어캐 하누
+
 /// find the minimum count of blocks to fit in given shape.
 fn solution(problem: &Vec<Vec<u8>>) -> usize {
-    let mut pixels = 0 as usize;
+    // let mut pixels = 0 as usize;
     for row in problem {
         for col in row {
-            if *col == 1 {
-                pixels = pixels + 1;
+            for block in &BLOCKS {
+                println!("{:?} {:?}", col, block)
             }
         }
     }
     // currently it returns pixels of shape
-    pixels
+    0
 }
 
 mod tests {
@@ -58,13 +71,12 @@ mod tests {
     
     #[test]
     fn count_pixel_of_shape() {
-        // 1 is represent the shape
         let problem = vec![
             vec![1, 1, 0, 0],
             vec![0, 1, 0, 0],
             vec![0, 1, 1, 1],
             vec![0, 1, 0, 0]
         ];
-        assert_eq!(solution(&problem), 7);
+        assert_eq!(solution(&problem), 3);
     }
 }
