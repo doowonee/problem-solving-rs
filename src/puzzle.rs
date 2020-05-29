faa17b5717e3dfbb489a861789b03f0c6e5ba306
the1900 <the1900.me@gmail.com>
2020-05-29 16:09:50 +0900

    change expression of shape and block
    2d array to list for respenting shape.

--- src/puzzle.rs
+++ src/puzzle.rs
@@ -39,18 +39,51 @@
 //! ```
 //! 
 
-/// find the minimum count of blocks to fit in given shape.
-fn solution(problem: &Vec<Vec<u8>>) -> usize {
-    let mut pixels = 0 as usize;
-    for row in problem {
-        for col in row {
-            if *col == 1 {
-                pixels = pixels + 1;
-            }
-        }
+/// vertex of block which is coordinated system.
+struct Vertex(u32, u32);
+
+/// show only coordinate point without sturct name
+impl std::fmt::Debug for Vertex {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("")
+        .field(&self.0)
+        .field(&self.1)
+        .finish()
     }
-    // currently it returns pixels of shape
-    pixels
+}
+
+/// list of vertex which means shape of the block.
+struct Block {
+    vertices: Vec<Vertex>,
+}
+
+/// basically Shape is list of vertices but It's not a Block
+type Shape = Vec<Vertex>;
+
+/// show only list of vertex without sturct name
+impl std::fmt::Debug for Block {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_list().entries(&self.vertices).finish()
+    }
+}
+
+/// find the minimum count of blocks to fit in given shape.
+fn solution(problem: &Shape) -> usize {
+    // Blocks which can fit in the given shape.
+    let block_pool: [Block; 5] = [
+        Block { vertices: vec![Vertex(0,0)] },
+        Block { vertices: vec![Vertex(0,0), Vertex(1,0)] },
+        Block { vertices: vec![Vertex(0,0), Vertex(1,0), Vertex(2,0)] },
+        Block { vertices: vec![Vertex(0,0), Vertex(1,0), Vertex(2,0), Vertex(3,0)] },
+        Block { vertices: vec![Vertex(0,0), Vertex(1,0), Vertex(1,1), Vertex(0,1)] },
+    ];
+    
+    println!("given shape {:?}", problem);
+    for block in &block_pool {
+        println!("your have: {:?}", block);
+    }
+    // currently it returns wrong number
+    0
 }
 
 mod tests {
@@ -58,12 +91,11 @@ mod tests {
     
     #[test]
     fn count_pixel_of_shape() {
-        // 1 is represent the shape
-        let problem = vec![
-            vec![1, 1, 0, 0],
-            vec![0, 1, 0, 0],
-            vec![0, 1, 1, 1],
-            vec![0, 1, 0, 0]
+        let problem = vec![ 
+            Vertex(0,0), 
+            Vertex(1,0), Vertex(1,1), Vertex(1,2), Vertex(1,3),
+            Vertex(2,2), 
+            Vertex(3,2),
         ];
         assert_eq!(solution(&problem), 7);
     }
